10e30d64964ec329da9eda9b824b53c9b4fe1eb4
S David Kong <SDAVIDKONG@gmail.com>
2022-09-12 22:47:43 -0400

    init repo

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+
+const RPC_URL_GOERLI = process.env.RPC_URL_GOERLI;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  // defaultNetwork: "hardhat",
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: RPC_URL_GOERLI,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const { ethers, run, network } = require("hardhat");
+
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("Deploying contract...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  console.log(`Deployed contract to ${simpleStorage.address}`);
+  console.log(network.config);
+
+  const currentValue = await simpleStorage.retrieve();
+  console.log(`Current Value is: ${currentValue}`);
+
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log(`Updated value is: ${updatedValue}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
